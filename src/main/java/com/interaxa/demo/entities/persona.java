75966d9cfee5cb22b8ff773b4892e4a22e80e717
Julián Pepa Degani <julianpepadegani97@gmail.com>
2022-06-29 10:12:28 -0300

    Clase persona: creada

--- src/main/java/com/interaxa/demo/entities/persona.java
+++ src/main/java/com/interaxa/demo/entities/persona.java
@@ -1,18 +1,39 @@
-package main.java.com.interaxa.demo.entities;
+package com.interaxa.demo.entities;
 
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "personas")
 public class persona {
+    
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
-    private String name;
+
+    @Column(name = "nombre")
+    private String nombre;
+    @Column(name = "id")
     private String apellido;
+    @Column(name = "id")
     private long dni;
+    @Column(name = "id")
     private boolean empleado;
 
+    public persona() {}
 
+    public persona(String nombre, String apellido, long dni, boolean empleado) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.empleado = empleado;
+    }
 
     public long getId() {return id;}
 
-    public String getName() {return name;}
-    public void setName(String name) {this.name = name;}
+    public String getNombre() {return nombre;}
+    public void setNombre(String nombre) {this.nombre = nombre;}
 
     public String getApellido() {return apellido;}
     public void setApellido(String apellido) {this.apellido = apellido;}
@@ -22,5 +43,6 @@ public class persona {
 
     public boolean isEmpleado() {return empleado;}
     public void setEmpleado(boolean empleado) {this.empleado = empleado;}
+
 
 }
